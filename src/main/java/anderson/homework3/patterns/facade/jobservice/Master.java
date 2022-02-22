4de6640d556e318e5babed4e2f4421fd67377229
AllexBolt <bolt.izh@mail.ru>
2022-02-22 18:40:42 +0300

    add patterns facade and flyweight

---
+++ src/main/java/anderson/homework3/patterns/facade/jobservice/Master.java
@@ -0,0 +1,12 @@
+package anderson.homework3.patterns.facade.jobservice;
+
+
+public class Master {
+    public void doJobMaster(SparePart sparePart){
+        if (sparePart.isSparePart()){
+            System.out.println("Car is repair");
+        }else {
+            System.out.println("Master is out of work..");
+        }
+    }
+}
