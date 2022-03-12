797adee8b24414367f52721db1f1c76055d28a9b
v.punko <Corvet23>
2022-03-12 17:17:05 +0300

    added presentList.html and get presented students

--- src/main/java/andersen/randomize/service/StudentService.java
+++ src/main/java/andersen/randomize/service/StudentService.java
@@ -1,4 +1,11 @@
 package andersen.randomize.service;
 
+import andersen.randomize.entity.Student;
+import andersen.randomize.service.wrapper.StudentListWrapper;
+
+import java.util.List;
+
 public interface StudentService {
+
+    List<Student> getPresentedStudentById(StudentListWrapper studentWrapper);
 }
--- src/main/java/andersen/randomize/service/StudentServiceImpl.java
+++ src/main/java/andersen/randomize/service/StudentServiceImpl.java
@@ -1,7 +1,13 @@
 package andersen.randomize.service;
 
 import andersen.randomize.dao.StudentRepository;
+import andersen.randomize.entity.Student;
+import andersen.randomize.service.wrapper.StudentListWrapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -11,4 +17,16 @@ public class StudentServiceImpl implements StudentService {
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+
+    public List<Student> getPresentedStudentById(StudentListWrapper studentWrapper) {
+        List<Student> studentsOnlyWithId = studentWrapper.getStudents().stream().filter(s -> s.getId() > 0).collect(Collectors.toList());
+        List<Student> presentedToday = new ArrayList<>();
+        for (Student s : studentsOnlyWithId) {
+            presentedToday.add(studentRepository.findById(s.getId()).orElse(new Student()));
+        }
+
+        return presentedToday;
+    }
+
+
 }
