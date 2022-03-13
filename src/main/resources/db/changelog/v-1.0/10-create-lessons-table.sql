a2a5008092276789068ff5ef2cd5feed64b9f248
SergeyOrlovskiy13 <96080613+SergeyOrlovskiy13@users.noreply.github.com>
2022-03-13 21:21:58 +0200

    add liquibase into project

---
+++ src/main/resources/db/changelog/v-1.0/10-create-lessons-table.sql
@@ -0,0 +1,5 @@
+create table lessons
+(
+    id 		   int primary key auto_increment,
+    date date  not null
+);
---
+++ src/main/resources/db/changelog/v-1.0/20-drop-student_lessons-table.sql
@@ -0,0 +1,8 @@
+alter table student_lessons
+            drop lesson_student__id_fk;
+
+GO
+
+drop table student_lessons
+
+GO
